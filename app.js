afe60513364b42cbe6dabb1078e10801ed055987
elidor8686 <elidor8686@gmail.com>
2016-05-18 10:03:12 +0300

    fors

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var winston = require('./lib/winston');
 //var db = require('./lib/db');
 
 
-
 var express = require('express');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
@@ -18,30 +17,6 @@ var tracking = require('./routes/tracking');
 var app = express();
 app.disable('etag');
 
-app.use(function (req, res, next) {
-
-
-    try {
-        var ip =
-            req.headers['fastly-client-ip'] ||
-            req.headers['x-appengine-user-ip'] ||
-            req.headers['x-forwarded-for'] ||
-            req.headers['x-real-ip'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress;
-
-        if (ip) {
-            req.remoteIp = ip.replace("::ffff:", "");
-        }
-    } catch (e) {
-        console.log("set ip error", e);
-    }
-
-    next();
-
-});
-
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
@@ -53,7 +28,7 @@ app.use(function (req, res, next) {
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: false}));
 //app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/tracking', tracking);
