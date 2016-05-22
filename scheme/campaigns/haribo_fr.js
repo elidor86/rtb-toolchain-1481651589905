325a3d938a332abf39c5d46fba281b922449164b
elidor8686 <elidor8686@gmail.com>
2016-05-22 19:35:46 +0300

    fors

---
+++ scheme/campaigns/haribo_fr.js
@@ -0,0 +1,16 @@
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 2,
+    "Campaign_Name": "Sweepstakes-mb",
+    "BID_Value": 0.005,
+    "Redirect_URL": "http://www.mb103.com/lnk.asp?o=9536&c=918271&a=191204",
+    "Max_Num_Of_BIDs": 3000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(fr)",
+    "Filter_URLs": "()",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": ""
+}
