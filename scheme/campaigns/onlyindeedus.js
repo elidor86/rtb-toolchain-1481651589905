f119071336be7112e442d89f0cb26944ee257fca
elidor8686 <elidor8686@gmail.com>
2016-05-27 11:53:54 +0300

    fors

---
+++ scheme/campaigns/onlyindeedus.js
@@ -0,0 +1,34 @@
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 220,
+    "Campaign_Name": "indeed-us",
+    "BID_Value": 0.005,
+    "Redirect_URL": "https://target-talent.com/campaigns/jobs/suggest?ch=C220&provider=2",
+    "Max_Num_Of_BIDs": 1000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us)",
+    "Filter_URLs": "(indeed)",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": ""
+}
+
+
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 300,
+    "Campaign_Name": "indeed-us",
+    "BID_Value": 0.005,
+    "Redirect_URL": "http://www.mb103.com/lnk.asp?o=9577&c=918271&a=191204",
+    "Max_Num_Of_BIDs": 5000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us)",
+    "Filter_URLs": "(netflix)",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": ""
+}
