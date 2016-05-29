8649c62a39c25f85b130ba2453e9c96f60e8ff85
elidor8686 <elidor8686@gmail.com>
2016-05-30 00:02:49 +0300

    fors

--- scheme/campaigns/onlyindeedus.js
+++ scheme/campaigns/onlyindeedus.js
@@ -86,3 +86,41 @@ var a = {
     "counter": "",
     "BuyingList": "jobseekers"
 }
+
+
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 224,
+    "Campaign_Name": "Gaming-us",
+    "BID_Value": 0.003,
+    "Redirect_URL": "http://www.mb103.com/lnk.asp?o=8689&c=918271&a=191204",
+    "Max_Num_Of_BIDs": 30000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us)",
+    "Filter_URLs": "()",
+    "Filter_KeyWords": "(mmo|rpg games|mmorpg)",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": "",
+    "BuyingList": "jobseekers"
+}
+
+
+var a= {
+    "Channel_ID": 2,
+    "Campaign_ID": 225,
+    "Campaign_Name": "indeed-us",
+    "BID_Value": 0.015,
+    "Redirect_URL": "https://target-talent.com/campaigns/jobs/suggest?ch=C225&provider=2",
+    "Max_Num_Of_BIDs": 30000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us)",
+    "Filter_URLs": "(indeed)",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": "",
+    "BuyingList": "jobseekers"
+}
