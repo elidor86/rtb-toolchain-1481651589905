353f1f274307b9e0b4a8aa88944843aab91a633c
elidor8686 <elidor8686@gmail.com>
2016-05-29 22:30:44 +0300

    fors

--- scheme/campaigns/onlyindeedus.js
+++ scheme/campaigns/onlyindeedus.js
@@ -50,3 +50,39 @@ var a = {
     "Num_Of_BIDS_Won": 0,
     "counter": ""
 }
+
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 221,
+    "Campaign_Name": "job-seekers-us",
+    "BID_Value": 0.01,
+    "Redirect_URL": "https://target-talent.com/campaigns/jobs/suggest?ch=C221&provider=2",
+    "Max_Num_Of_BIDs": 30000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us)",
+    "Filter_URLs": "()",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": "",
+    "BuyingList": "jobseekers"
+}
+
+var a = {
+    "Channel_ID": 2,
+    "Campaign_ID": 223,
+    "Campaign_Name": "job-seekers-gb",
+    "BID_Value": 0.002,
+    "Redirect_URL": "https://target-talent.com/campaigns/jobs/suggest?ch=C221&provider=2",
+    "Max_Num_Of_BIDs": 30000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(gb)",
+    "Filter_URLs": "()",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0,
+    "counter": "",
+    "BuyingList": "jobseekers"
+}
