05df04aaaf8f5b139a4947d5ecf8b133b2a017fa
elidor8686 <elidor8686@gmail.com>
2016-06-01 12:37:27 +0300

    fors

--- scheme/campaigns/onlyindeedus.js
+++ scheme/campaigns/onlyindeedus.js
@@ -161,11 +161,26 @@ var a = {
 }
 
 
+a={
+    "Channel_ID": 2,
+    "Campaign_ID": 232,
+    "Campaign_Name": "vhm-us-de-fr-uk",
+    "BID_Value": 0.001,
+    "Redirect_URL": "https://target-talent.com/campaigns/jobs/vhm",
+    "Max_Num_Of_BIDs": 5000,
+    "Bid_URL": "http://rtb.dans-leads.com/tracking?Campaign_ID=@@Campaign_ID@@&BID_ID=@@BID_ID@@",
+    "Filter_GEO": "(us|uk|fr|gb|de)",
+    "Filter_URLs": "()",
+    "Filter_KeyWords": "()",
+    "Filter_Function": "vhm",
+    "Num_Of_BIDs_Sent": 0,
+    "Num_Of_BIDS_Won": 0
+}
 
 var a = {
     "Channel_ID": 2,
-    "Campaign_ID": 231,
-    "Campaign_Name": "maya-il",
+    "Campaign_ID": 232,
+    "Campaign_Name": "vhm-il",
     "BID_Value": 0.001,
     "Redirect_URL": "http://www.stampready.net/dashboard/online/index.php?id=93624",
     "Max_Num_Of_BIDs": 25000,
