a69a6e1df3ab8886b4dfde47de59c6e796928720
elidor8686 <elidor8686@gmail.com>
2016-11-13 14:12:14 +0200

    fors

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var Bidder = require('../lib/bidder');
+var redirect = require('../lib/redirect');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/redirect/:id', redirect.redirect);
 router.get('/ch/:id', Bidder.bidder);
 router.get('/env', function (req, res) {
     return res.json(process.env);
